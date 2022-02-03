250f352c4420d23c3fa6ab360aa7ed02c911bcba
Christian D'Angelo <christiandangelo@gmail.com>
2022-02-03 20:38:38 -0300

    entrega Redux & state2

--- src/store/reducer/product.reducer.js
+++ src/store/reducer/product.reducer.js
@@ -1,23 +1,25 @@
-import {PRODUCTS}  from '../../utils/data/products'
-import { SELECTED_PRODUCT, FILTER_PRODUCT} from '../actions/product.action'
+import { PRODUCTS } from '../../utils/data/products'
+import { SELECTED_PRODUCT, FILTER_PRODUCT } from '../actions/product.action'
 const initialState = {
-    products: PRODUCTS ,
+    products: PRODUCTS,
     filteredProduct: [],
     selected: null
 }
 
 const ProductReducer = (state = initialState, action) => {
-    switch(action.type){
+    switch (action.type) {
         case SELECTED_PRODUCT:
-            return{
+            return {
                 ...state,
                 selected: state.products.find(product => product.id === action.productId)
             }
         case FILTER_PRODUCT:
-            return{
+            return {
                 ...state,
                 filterProduct: state.product.filter(product => product.category === action.categoryId)
             }
+        default:
+            return state
     }
 }
 
